d6d33055e8c7a3b67af2deb1e019fc26ff2b1183
chimingkuei <chimingkuei@gmail.com>
2025-07-14 12:38:31 +0800

    AppLauncher

---
+++ AppLauncher/MainWindow.xaml.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppLauncher
+{
+    #region Config Class
+    public class SerialNumber
+    {
+        [JsonProperty("Parameter1_val")]
+        public string Parameter1_val { get; set; }
+        [JsonProperty("Parameter2_val")]
+        public string Parameter2_val { get; set; }
+    }
+
+    public class Model
+    {
+        [JsonProperty("SerialNumbers")]
+        public SerialNumber SerialNumbers { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("Models")]
+        public List<Model> Models { get; set; }
+    }
+    #endregion
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region Config
+        private SerialNumber SerialNumberClass()
+        {
+            SerialNumber serialnumber_ = new SerialNumber
+            {
+                Parameter1_val = Parameter1.Text,
+                Parameter2_val = Parameter2.Text
+            };
+            return serialnumber_;
+        }
+
+        private void LoadConfig(int model, int serialnumber, bool encryption = false)
+        {
+            List<RootObject> Parameter_info = Config.Load(encryption);
+            if (Parameter_info != null)
+            {
+                Parameter1.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter1_val;
+                Parameter2.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter2_val;
+            }
+            else
+            {
+                // 結構:2個Models、Models下在各2個SerialNumbers
+                SerialNumber serialnumber_ = SerialNumberClass();
+                List<Model> models = new List<Model>
+                {
+                    new Model { SerialNumbers = serialnumber_ },
+                    new Model { SerialNumbers = serialnumber_ }
+                };
+                List<RootObject> rootObjects = new List<RootObject>
+                {
+                    new RootObject { Models = models },
+                    new RootObject { Models = models }
+                };
+                Config.SaveInit(rootObjects, encryption);
+            }
+        }
+
+        private void SaveConfig(int model, int serialnumber, bool encryption = false)
+        {
+            Config.Save(model, serialnumber, SerialNumberClass(), encryption);
+        }
+        #endregion
+
+        #region Dispatcher Invoke 
+        public string DispatcherGetValue(System.Windows.Controls.TextBox control)
+        {
+            string content = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                content = control.Text;
+            });
+            return content;
+        }
+
+        public void DispatcherSetValue(string content, System.Windows.Controls.TextBox control)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                control.Text = content;
+            });
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig(0, 0);
+        }
+        BaseConfig<RootObject> Config = new BaseConfig<RootObject>();
+        BaseLogRecord Logger = new BaseLogRecord();
+        AppLaunchHandler ALH = new AppLaunchHandler();
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as System.Windows.Controls.Button).Name)
+            {
+                case nameof(Demo):
+                    {
+                        string vlcPath = @"D:\vlc-3.0.21\vlc.exe";
+                        string rtspUrl = "rtsp://admin0930:Asher19910930@192.168.0.130:554/stream2";
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            FileName = vlcPath,
+                            Arguments = $"\"{rtspUrl}\"",
+                            UseShellExecute = false
+                        };
+                        Process.Start(startInfo);
+                        Thread.Sleep(1000);
+                        ALH.ActivateWindow("rtsp://192.168.0.130:554/stream2 - VLC 媒體播放器");
+                        System.Drawing.Rectangle position = new System.Drawing.Rectangle(100, 100, 200, 200); 
+                        ALH.SetWindowsPosWrapper("rtsp://192.168.0.130:554/stream2 - VLC 媒體播放器", position);
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+
+    }
+}
